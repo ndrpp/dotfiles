d306bea251d7f191ad8e7fe04afc14850bef6d27
ndrpp <popandrei230@yahoo.com>
2024-07-16 20:48:02 +0300

    Add wallpaper, add more packages to nix

--- configuration.nix
+++ configuration.nix
@@ -5,146 +5,167 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    imports =
+        [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+        ];
+
+# Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.configurationLimit = 5;
+    nix.gc.automatic = true;
+    nix.gc.dates = "daily";
+    nix.gc.options = "--delete-older-than 7d";
+
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+# Enable networking
+        networking.networkmanager.enable = true;
+
+# Set your time zone.
+    time.timeZone = "Europe/Bucharest";
+
+# Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "ro_RO.UTF-8";
+        LC_IDENTIFICATION = "ro_RO.UTF-8";
+        LC_MEASUREMENT = "ro_RO.UTF-8";
+        LC_MONETARY = "ro_RO.UTF-8";
+        LC_NAME = "ro_RO.UTF-8";
+        LC_NUMERIC = "ro_RO.UTF-8";
+        LC_PAPER = "ro_RO.UTF-8";
+        LC_TELEPHONE = "ro_RO.UTF-8";
+        LC_TIME = "ro_RO.UTF-8";
+    };
+
+# Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+# Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.displayManager.defaultSession = "none+i3";
+    services.xserver.desktopManager.gnome.enable = true;
+
+# Configure keymap in X11
+    services.xserver = {
+        xkb.layout = "us";
+        xkb.variant = "";
+
+
+        windowManager.i3 = {
+            enable = true;
+            extraPackages = with pkgs; [
+                dmenu
+                    dunst
+                    i3status
+                    i3lock
+            ];
+        };
+    };
+
+# Enable CUPS to print documents.
+    services.printing.enable = true;
+
+# Enable sound with pipewire.
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+# If you want to use JACK applications, uncomment this
+#jack.enable = true;
+
+# use the example session manager (no others are packaged yet so this is enabled by default,
+# no need to redefine it in your config for now)
+#media-session.enable = true;
+    };
+
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.andrei = {
+        isNormalUser = true;
+        description = "Andrei";
+        extraGroups = [ "networkmanager" "wheel" ];
+    };
+
+# Install firefox.
+    programs.firefox.enable = true;
+
+# Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    fonts.packages = with pkgs; [
+        nerdfonts
+            noto-fonts
+            noto-fonts-cjk
+            noto-fonts-emoji
+            liberation_ttf
+            fira-code
+            fira-code-symbols
+            mplus-outline-fonts.githubRelease
+            dina-font
+            proggyfonts
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Bucharest";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ro_RO.UTF-8";
-    LC_IDENTIFICATION = "ro_RO.UTF-8";
-    LC_MEASUREMENT = "ro_RO.UTF-8";
-    LC_MONETARY = "ro_RO.UTF-8";
-    LC_NAME = "ro_RO.UTF-8";
-    LC_NUMERIC = "ro_RO.UTF-8";
-    LC_PAPER = "ro_RO.UTF-8";
-    LC_TELEPHONE = "ro_RO.UTF-8";
-    LC_TIME = "ro_RO.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.displayManager.defaultSession = "none+i3";
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-
-
-    windowManager.i3 = {
-    	enable = true;
-	extraPackages = with pkgs; [
-		dmenu
-		dunst
-		i3status
-		i3lock
-	];
-    };
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.andrei = {
-    isNormalUser = true;
-    description = "Andrei";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
+# List packages installed in system profile. To search, run:
 # $ nix search wget
-  environment.systemPackages = with pkgs; [
-	              awscli2
-                  cargo
-                  chromium
-                  clang
-                  curl
-                  discord
-                  docker
-                  fastfetch
-                  firefox
-                  fd
-                  fzf
-                  gcc
-                  git
-                  gnumake
-                  go
-                  jdk
-                  kitty
-                  libreoffice
-                  lua
-                  luarocks
-                  neovim
-                  nerdfonts
-                  nodejs
-                  python3
-                  qbittorrent
-                  ripgrep
-                  rustc
-                  signal-desktop
-                  templ
-                  tenv
-                  tmux
-                  unzip
-                  vim
-                  vlc
-                  wget
-                  zig
-                  ];
+    environment.systemPackages = with pkgs; [
+        awscli2
+            cargo
+            chromium
+            clang
+            curl
+            discord
+            docker
+            fastfetch
+            feh
+            firefox
+            fd
+            flameshot
+            fzf
+            gcc
+            git
+            gnumake
+            go
+            jdk
+            kitty
+            libreoffice
+            lua
+            luarocks
+            neovim
+            nerdfonts
+            nodejs
+            pavucontrol
+            python3
+            qbittorrent
+            ripgrep
+            rustc
+            signal-desktop
+            templ
+            tenv
+            tmux
+            unzip
+            vim
+            vlc
+            wget
+            zig
+            ];
 
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
+    programs.neovim.enable = true;
+    programs.neovim.defaultEditor = true;
 # Some programs need SUID wrappers, can be configured further or are
 # started in user sessions.
 # programs.mtr.enable = true;
@@ -156,7 +177,7 @@
 # List services that you want to enable:
 
 # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+    services.openssh.enable = true;
 
 # Open ports in the firewall.
 # networking.firewall.allowedTCPPorts = [ ... ];
@@ -170,6 +191,6 @@
 # this value at the release version of the first install of this system.
 # Before changing this value read the documentation for this option
 # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+    system.stateVersion = "24.05"; # Did you read the comment?
 
 }
